47a015c0241e483c957a0412d1da268334407db0
admin <admin@YD2-19>
2022-04-11 14:10:00 +0900

    비교문

---
+++ HelloWorld/src/co/edu/variable/IfExample.java
@@ -0,0 +1,17 @@
+package co.edu.variable;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int myAge = 23;
+		boolean over20;
+		over20 = myAge > 20;
+		if(over20) {
+			System.out.println("over 20");
+		}
+		else {
+			System.out.println("under 20");
+		}
+	}
+
+}
