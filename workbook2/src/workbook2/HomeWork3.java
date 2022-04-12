ce5497405e513f33f4a7c5702aed64f77612d915
mus56 <mus56@DESKTOP-T8NCGN0>
2022-04-13 00:16:48 +0900

    숙제

---
+++ workbook2/src/workbook2/HomeWork3.java
@@ -0,0 +1,11 @@
+package workbook2;
+
+public class HomeWork3 {
+
+	public static void main(String[] args) {
+
+		int randomVal = (int)(Math.random()*3)+1;
+		System.out.println(randomVal);
+	}
+
+}
